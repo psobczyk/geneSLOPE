d195eb25e6a0c8e6ae0873d5ac7f9a303d8df72b
Piotr Sobczyk <sobbombo@gmail.com>
2015-10-09 17:39:24 -0700

    removing dependency on adegenet

--- R/readPLINK.R
+++ R/readPLINK.R
@@ -1,53 +1,3 @@
-#' Reading PLINK .raw files
-#'
-#' Reading PLINK files that were previously suitably tranformed
-#' in plink - see details
-#'
-#' @param file name of .raw file
-#'
-#' @return list \itemize{
-#' \item snps matrix of SNPs
-#' \item phenotype vector with phenotype values
-#' \item positions vector with information about SNP positions in chromosome
-#' }
-#'
-#' @details \strong{Exporting data from PLINK}
-#' As stated in documentation for function \code{\link[adegenet]{read.PLINK}},
-#' to import data to R, it needs to be exported from
-#' PLINK using the option "--recodeA" (and NOT "--recodeAD").
-#' The PLINK command should therefore look like:
-#' \code{plink --file data --recodeA}.
-#' For more information on this topic, please look at this webpage:
-#' \url{http://pngu.mgh.harvard.edu/~purcell/plink/dataman.shtml}
-#'
-readPLINK <- function(file){
-  splittedName <- strsplit(file, "\\.")[[1]]
-  if(tail(splittedName,1)!="raw")
-    error(paste0("Error. ", file, " - .raw file is required."))
-  file = splittedName[1]
-  rawFile <- paste0(file, ".raw")
-  if(!file.exists(rawFile))
-    error("No .raw file. Cannot proceed")
-  mapFile <- paste0(file, ".map")
-  if(file.exists(mapFile)){
-    snpData <- read.PLINK(file = rawFile, map.file = mapFile)
-  } else {
-    snpData <- read.PLINK(rawFile)
-  }
-
-  # we extract information about snp positions, phenotype and snps
-  positions <- snpData$other$position
-  phenotype <- snpData$other$phenotype
-  snps <- sapply(snpData$gen, function(x) as.integer(x))
-  result <- list(
-    snps = snps,
-    phenotype = phenotype,
-    positions = positions
-  )
-  return(result)
-}
-
-
 #' Reading SNPs from one PLINK .raw files
 #'
 #' Reading PLINK files that were previously suitably tranformed
@@ -70,7 +20,7 @@ readPLINK <- function(file){
 #' For more information, please refer to:
 #' \url{http://pngu.mgh.harvard.edu/~purcell/plink/dataman.shtml}
 #'
-readPLINK2 <- function(file){
+readPLINK <- function(file){
   # splittedName <- strsplit(file, "\\.")[[1]]
 #   if(tail(splittedName,1)!="raw")
 #     stop(paste0("Error. ", file, " - .raw file is required."))
@@ -78,7 +28,7 @@ readPLINK2 <- function(file){
 #   rawFile <- paste0(file, ".raw")
 #   if(!file.exists(rawFile))
 #     stop("No .raw file. Cannot proceed")
-  data_mat <- fread(file, stringsAsFactors = FALSE, header = TRUE)
+  data_mat <- data.table::fread(file, stringsAsFactors = FALSE, header = TRUE)
   info <- data_mat[,1:6,with=FALSE]
   data_mat <- data_mat[, !(1:6), with=FALSE]
   splittedName <- strsplit(file, "\\.")[[1]]
