308270c794d82c69929f00c5315e62552506751d
Piotr Sobczyk <sobbombo@gmail.com>
2015-10-19 20:02:55 -0700

    adding function to read and filter big .tped files

--- R/auxilliary.R
+++ R/auxilliary.R
@@ -13,3 +13,21 @@ replace_na_with_mean <- function(x) {
   x_bar <- mean(x, na.rm = TRUE)
   ifelse(is.na(x), x_bar, x)
 }
+
+# recoding snps as was described in
+# http://pngu.mgh.harvard.edu/~purcell/plink/dataman.shtml#recode
+recodeAD <- function(x){
+  sapply(seq(1, length(x), 2), function(i){
+    if(any(is.na(x[c(i,i+1)])))
+      return(c(NA,NA))
+    if(any(x[c(i,i+1)]==0))
+      return(c(NA,NA))
+    if(x[i]==1 & x[i+1]==1){
+      c(0,0)
+    } else if(x[i]==2 & x[i+1]==2){
+      c(2,0)
+    } else{
+      c(1,1)
+    }
+  })
+}
---
+++ R/readSNPs.R
@@ -0,0 +1,82 @@
+#' Reading SNPs from one PLINK .tped and .tfam files
+#'
+#' Reading PLINK files that were previously suitably tranformed
+#' in plink - see details
+#'
+#' @export
+#' @param file name of .tped file
+#' @param y phenotype
+#' @param pValMax p-value threshold value used for screening
+#' @param chunk.size size of chunk. The bigger the chunk the faster function works but
+#' computer might run out of RAM
+#' @param verbose should info be present
+#' @return list \itemize{
+#' \item X matrix of filtered SNPs
+#' \item y phenotype
+#' \item X_info SNPs info
+#' \item numberOfSnps number of snps read from .tped file
+#' \item pValMax p-value threshold value used for screening
+#' }
+#'
+#' @details \strong{Exporting data from PLINK}
+#' To import data to R, it needs to be exported from
+#' PLINK using the option "--recode12 --transposed"
+#' The PLINK command should therefore look like
+#' \code{plink --file data --recode12 --transposed --out temp}.
+#' For more information, please refer to:
+#' \url{http://pngu.mgh.harvard.edu/~purcell/plink/dataman.shtml}
+#'
+readSNPs <- function(file, y, pValMax = 0.05, chunk.size = 5e4, verbose = TRUE){
+  done = FALSE
+  chunk = 1
+  numberOfSnps <- 0
+  selectedSnpsNumbers <- NULL
+  X <- NULL
+  X_info <- NULL
+  y <- unname(y)
+  suma = sum((y-mean(y))^2)
+  n = length(y) - 2
+  message("Reading and screening data. Depending on data size this might take
+           several minutes")
+  while(!done)
+  {
+    temp = data.table::fread(file,skip=(chunk-1)*chunk.size+1,nrow=chunk.size,
+                             header = FALSE)
+    temp <- as.data.frame(temp)
+    SNPinfo <- t(temp[, (1:4)])
+    SNPinfo <- SNPinfo[,rep(1:ncol(SNPinfo), each=2)]
+    temp <- temp[, -(1:4)]
+    temp <- apply(temp, 1, function(row) {
+      a <- recodeAD(row)
+      c(a[1,], a[2,])
+    })
+    temp2 <- matrix(nrow = nrow(temp)/2, ncol = 2*ncol(temp))
+    temp2[,seq(1, 2*ncol(temp), 2)] <- temp[1:(nrow(temp)/2),]
+    temp2[,seq(2, 2*ncol(temp), 2)] <- temp[(nrow(temp)/2+1):nrow(temp),]
+    temp <- temp2
+    rm("temp2")
+    temp <- apply(temp, 2, replace_na_with_mean)
+    pVals <- apply(temp, 2,
+                   function(x) pValComp(x,y,n,suma))
+    smallPVals <- pVals<pValMax
+    X <- cbind(X, temp[,smallPVals])
+    numberOfSnps <- numberOfSnps + ncol(temp)
+    selectedSnpsNumbers <- c(selectedSnpsNumbers,
+                             2*(chunk-1)*chunk.size+which(smallPVals))
+    SNPinfo <- SNPinfo[,smallPVals]
+    X_info <- cbind(X_info, SNPinfo)
+    message(paste(chunk*chunk.size, "SNPs processed"))
+    chunk = chunk + 1
+    if(ncol(temp)/2<chunk.size) done = TRUE
+  }
+  #returning screening result
+  result <- structure( list(
+    X = X,
+    y = y,
+    X_info = X_info,
+    numberOfSnps = numberOfSnps,
+    pValMax = pValMax),
+    class="screeningResult")
+  return(result)
+}
+
