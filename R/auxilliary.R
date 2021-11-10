59ed32a4cd71f1057d56b1a13c7fa037f048fad6
Piotr Sobczyk <psobczyk@users.noreply.github.com>
2021-11-10 10:13:58 +0100

    switching to upgraded slope r package (#3)
    * using upgraded slope library
    
    * fixing plots and interpreting slope results
    
    * proper scaling of lambda sequence, small fixes in documentation
    
    Co-authored-by: piotr.sobczyk <piotr.sobczyk@olx.com>

--- R/auxilliary.R
+++ R/auxilliary.R
@@ -46,11 +46,10 @@ create_clumping_plot_data <- function(x){
     plot.data$snp <- plot.data$bp
   } else {
     chromosomes_limits <- aggregate(x$X_info[,3], list(x$X_info[,1]), max)
-    #granice_max <- cumsum(chromosomes_limits$x)
-    chromosomes_limits$x <- c(0,head(cumsum(chromosomes_limits$x),-1))
-    for(i in seq_along(unique(plot.data$chromosome))){
-      chromosome_idx <- unique(plot.data$chromosome)[i]
-      plot.data$snp[plot.data$chromosome==chromosome_idx] <- chromosomes_limits$x[i] +
+    chromosomes_limits$x <- c(0, head(cumsum(chromosomes_limits$x), -1))
+    for(i in seq_along(sort(unique(plot.data$chromosome)))){
+      chromosome_idx <- sort(unique(plot.data$chromosome))[i]
+      plot.data$snp[plot.data$chromosome==chromosome_idx] <- chromosomes_limits$x[chromosome_idx] +
         plot.data$snp[plot.data$chromosome==chromosome_idx]
     }
   }
@@ -89,13 +88,6 @@ create_slope_plot_data <- function(x){
     }
   }
 
-  # limits <- aggregate(x$X_info[,3], list(x$X_info[,1]), max)
-  # limits_max <- cumsum(limits$x)
-  # limits$x <- c(0,head(cumsum(limits$x),-1))
-  # for(i in unique(plot.data$chromosome)){
-  #   plot.data$snp[plot.data$chromosome==i] <- limits$x[i] +
-  #     plot.data$snp[plot.data$chromosome==i]
-  # }
   plot.data$val[plot.data$representatives] <- (x$effects^2/var(x$y))
   plot.data
 }
--- R/select_snps.R
+++ R/select_snps.R
@@ -12,7 +12,7 @@
 #' @param type method for snp selection. slope (default value) is SLOPE
 #' on clump representatives, smt is Benjamini-Hochberg procedure on
 #' single marker test p-values for clump representatives
-#' @param lambda lambda for SLOPE. See \code{\link[SLOPE]{create_lambda}}
+#' @param lambda lambda for SLOPE. See \code{\link{create_lambda}}
 #' @param sigma numeric, sigma for SLOPE
 #' @param verbose logical, if TRUE progress bar is printed
 #' @return object of class \code{\link{selectionResult}}
@@ -21,8 +21,8 @@
 #' \dontrun{
 #' slope.result <- select_snps(clumping.result, fdr=0.1)
 #' }
-select_snps <- function(clumpingResult, fdr = 0.1, type=c("slope", "smt"),
-                        lambda="gaussian", sigma=NULL, verbose = TRUE){
+select_snps <- function(clumpingResult, fdr = 0.1, type = c("slope", "smt"),
+                        lambda = "gaussian", sigma = NULL, verbose = TRUE){
   if(fdr>=1 | fdr <= 0){
     stop("FDR has to be within range (0,1)")
   }
@@ -31,8 +31,8 @@ select_snps <- function(clumpingResult, fdr = 0.1, type=c("slope", "smt"),
          number of rows in X")
   }
 
-  if(type=="slope"){
-    lambda <- SLOPE::create_lambda(length(clumpingResult$y),
+  if(any(type=="slope")){
+    lambda <- create_lambda(length(clumpingResult$y),
                                    clumpingResult$numberOfSnps, fdr, "gaussian")
     lambda <- lambda[1:ncol(clumpingResult$X)]
     lambda_diffs <- diff(lambda)
@@ -40,26 +40,28 @@ select_snps <- function(clumpingResult, fdr = 0.1, type=c("slope", "smt"),
       warning("All lambdas are equal. SLOPE does not guarantee
             False Discovery Rate control")
     }
-
+    # lambdas from the previous SLOPE implementation have to be divided by the sqrt(n)
+    # as LASSO scaling of the loss function is used
+    lambda <- lambda / sqrt(nrow(clumpingResult$X))
+    
     if (is.null(sigma) && (nrow(clumpingResult$X) >= ncol(clumpingResult$X) + 30)) {
       selected = NULL
       repeat {
         selected.prev = selected
         sigma = c(sigma, estimate_noise(clumpingResult$X[, selected], clumpingResult$y))
-        result = SLOPE::SLOPE(clumpingResult$X, clumpingResult$y, fdr = fdr,
-                              lambda = lambda, sigma = tail(sigma, 1))
-        selected = result$selected
+        result = SLOPE::SLOPE(x = clumpingResult$X, y = clumpingResult$y,
+                              q = fdr, lambda = lambda, alpha = tail(sigma, 1))
+        selected = which(result$nonzeros)
         if (identical(selected, selected.prev))
           break
       }
       sigma = tail(sigma, 1)
     }
 
-    slopeResult <- SLOPE::SLOPE(X = clumpingResult$X, y = clumpingResult$y,
-                                fdr = fdr, lambda = lambda, sigma = sigma)
-
-    selectedSNPs <- unlist(clumpingResult$SNPnumber)[slopeResult$selected]
-    selected = slopeResult$selected
+    slopeResult <- SLOPE::SLOPE(x = clumpingResult$X, y = clumpingResult$y,
+                                q = fdr, lambda = lambda, alpha = sigma)
+    selected = which(slopeResult$nonzeros)
+    selectedSNPs <- unlist(clumpingResult$SNPnumber)[selected]
     selectedSNPs <- sort(selectedSNPs)
   } else if(type=="smt"){
     repPVals = clumpingResult$pVals[clumpingResult$selectedSnpsNumbers]
